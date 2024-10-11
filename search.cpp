a16e51a2958918f92a9d6dd5ec30d77e178af89a
Noah Graham <noehy@Noahs-MacBook-Air-4.local>
2024-10-11 16:48:48 -0500

    Remove unnecessary files

--- search.cpp
+++ search.cpp
@@ -9,68 +9,89 @@
 using namespace std;
 
 // Function to set the console to UTF-8
-void setConsoleUTF8() {
+void setConsoleUTF8() 
+{
     SetConsoleOutputCP(CP_UTF8);
 }
 
 // Function to read verses from a file
-vector<string> readVersesFromFile(const string& filename) {
+vector<string> readVersesFromFile(const string& filename) 
+{
     vector<string> verses;
     ifstream file(filename);
     string line;
-    if (file.is_open()) {
-        while (getline(file, line)) {
+    if (file.is_open()) 
+    {
+        while (getline(file, line)) 
+        {
             verses.push_back(line);
         }
         file.close();
-    } else {
+    } 
+    else 
+    {
         cerr << "Unable to open file: " << filename << endl;
     }
     return verses;
 }
 
 // Function to read search words from a file
-vector<string> readWordsFromFile(const string& filename) {
+vector<string> readWordsFromFile(const string& filename) 
+{
     vector<string> words;
     ifstream file(filename);
     string line;
-    if (file.is_open()) {
-        while (getline(file, line)) {
+    if (file.is_open()) 
+    {
+        while (getline(file, line)) 
+        {
             words.push_back(line);
         }
         file.close();
-    } else {
+    } 
+    else 
+    {
         cerr << "Unable to open file: " << filename << endl;
     }
     return words;
 }
 
 // Function to search for words in verses and print matching verses
-void searchWordsInVerses(const vector<string>& verses, const vector<string>& words) {
+void searchWordsInVerses(const vector<string>& verses, const vector<string>& words) 
+{
     unordered_map<string, vector<string>> wordVerseMap;
 
-    for (const auto& verse : verses) {
-        for (const auto& word : words) {
-            if (verse.find(word) != string::npos) {
+    for (const auto& verse : verses) 
+    {
+        for (const auto& word : words) 
+        {
+            if (verse.find(word) != string::npos) 
+            {
                 wordVerseMap[word].push_back(verse);
             }
         }
     }
 
-    for (const auto& word : words) {
+    for (const auto& word : words) 
+    {
         cout << "Verses containing the word \"" << word << "\":" << endl;
-        if (wordVerseMap.find(word) != wordVerseMap.end()) {
-            for (const auto& verse : wordVerseMap[word]) {
+        if (wordVerseMap.find(word) != wordVerseMap.end()) 
+        {
+            for (const auto& verse : wordVerseMap[word]) 
+            {
                 cout << verse << endl;
             }
-        } else {
+        } 
+        else 
+        {
             cout << "No verses found for the word \"" << word << "\"." << endl;
         }
         cout << endl;
     }
 }
 
-int main() {
+int main() 
+{
     setConsoleUTF8();  // Set console to UTF-8
 
     string versesFilename, wordsFilename;
@@ -90,10 +111,12 @@ int main() {
     cout << "Loaded " << words.size() << " words to search for." << endl;
 
     // Debugging: Print the first few verses and words to check content
-    if (!verses.empty()) {
+    if (!verses.empty()) 
+    {
         cout << "First verse loaded: " << verses[0] << endl;
     }
-    if (!words.empty()) {
+    if (!words.empty()) 
+    {
         cout << "First word to search for: " << words[0] << endl;
     }
 
